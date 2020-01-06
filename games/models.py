9516e39e97b5394e9f9d4cba59f5b885bc25d138
evgp <bratikha@gmail.com>
2020-01-06 21:28:35 +1200

    3

--- games/models.py
+++ games/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import Group, User
 
 # Create your models here.
+
+class Game(models.Model):
+    name = models.CharField(max_length=255)
+    author = models.ManyToManyField(User)
